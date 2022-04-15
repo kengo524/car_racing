3fd9455242556983cf0b17bb095271f609446395
kengo524 <kengo.konishi@tokyo.supersoftware.co.jp>
2022-04-15 15:03:57 +0900

    課題3リファクタリング

--- main3.php
+++ main3.php
@@ -4,43 +4,16 @@
     require_once 'classes/nissan.php';
     require_once 'classes/ferrari.php';
     
-    function randomCreate(){
+    $classNames = ["Honda","Nissan","Ferrari"];
+
+    foreach($classNames as $className){
+        $prices = [];
         $num = mt_rand(1,100);
         for($i = 0; $i < $num; $i++){
-            $honda_list[] = new Honda();
-            $nissan_list[] = new Nissan();
-            $ferrari_list[] = new Ferrari();
-        }
-        return $honda_list;
-        return $nissan_list;
-        return $ferrari_list;
+            $objects[] = new $className();
+        }  
     }
+    $all_objects = array_merge($objects);
+    cars::printStatistics($all_objects);
 
-    // 値段取得関数
-    function getPrices($car_object){
-        return $car_object->getPrices();
-    }
-
-    function printStatistics($car_list){
-        // 合計金額
-        $sum_price = array_sum(array_map('getPrices',$car_list));
-        // 合計台数
-        $sum_cars = count($car_list);
-        // 平均金額
-        $ave_price = round($sum_price / $sum_cars);
-
-        echo "合計金額：{$sum_price}万円";
-        echo "<br />"; 
-        echo "平均金額：{$ave_price}万円";
-        echo "<br />";  
-        echo "(参考：製造合計台数は{$sum_cars}台）";  
-    }
-
-    $honda_list = randomCreate();
-    $nissan_list = randomCreate();
-    $ferrari_list = randomCreate();
-
-    $created_cars = array_merge($honda_list,$nissan_list,$ferrari_list);
-
-    printStatistics($created_cars);
 ?>
