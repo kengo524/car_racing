0ade0d5cc01843398e0f99662695939492b95acb
kengo524 <kengo.konishi@tokyo.supersoftware.co.jp>
2022-04-18 12:10:34 +0900

    リファクタリング（all_objects,prices[]削除）

--- main3.php
+++ main3.php
@@ -7,13 +7,11 @@
     $classNames = ["Honda","Nissan","Ferrari"];
 
     foreach($classNames as $className){
-        $prices = [];
         $num = mt_rand(1,100);
         for($i = 0; $i < $num; $i++){
             $objects[] = new $className();
         }  
     }
-    $all_objects = array_merge($objects);
-    cars::printStatistics($all_objects);
+    cars::printStatistics($objects);
 
 ?>
